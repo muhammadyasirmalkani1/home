4ca0b0d7b3176e992a6042ddc6c1ae5b8c905184
Muhammad Yasir Malkani <muhammadyasirmalkani1@gmail.com>
2025-10-25 22:13:52 +0500

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,4 +1,3 @@
-// components/navigation.tsx
 import { useState, useEffect, useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { Button } from "@/components/ui/button";
